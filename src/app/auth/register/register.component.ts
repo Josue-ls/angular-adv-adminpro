8f6b4bd38a8ed21566f8332be23048d6de82324b
Josue Lopez <jslopez.ts@gmail.com>
2022-06-19 17:25:06 -0500

    feat: seccion 3 disenio

---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['../style/auth.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
