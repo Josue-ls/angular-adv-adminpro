74e1e18dfb3004356049f399451af2ad16925faf
Josue Lopez <jslopez.ts@gmail.com>
2022-06-25 22:03:40 -0500

    styles on input added

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,16 +1,22 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
   styles: [],
 })
-export class IncrementadorComponent {
+export class IncrementadorComponent implements OnInit {
   @Input('inValue') progressIn: number = 50;
+  @Input() btnClass: string = 'btn-primary';
+
   @Output('outValue') progressOut: EventEmitter<number> = new EventEmitter();
 
   // Se pueden tener tantos inputs como sean necesarios
   // @Input() input1: string | number | boolean | array | object;
+
+  ngOnInit() {
+    this.btnClass = `btn ${this.btnClass}`;
+  }
 
   changeValue(value: number): void {
     if (this.progressIn >= 100 && value > 0) {
@@ -26,4 +32,14 @@ export class IncrementadorComponent {
     this.progressIn += value;
     this.progressOut.emit(this.progressIn);
   }
+
+  onChange(value: number) {
+    if (value >= 100) this.progressIn = 100;
+    if (value <= 0) this.progressIn = 0;
+    if (value >= 0 && value <= 100) this.progressIn = value;
+    console.log(this.progressIn);
+    this.progressOut.emit(this.progressIn);
+  }
+
+  isInvalidClass = () => this.progressIn > 100 || this.progressIn < 0;
 }
